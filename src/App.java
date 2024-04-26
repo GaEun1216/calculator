691e79e4739aa62c8bf7eaa27398b46261415148
GaEun1216 <gani6382@gmail.com>
2024-04-26 16:39:31 +0900

    Level 1-4

--- src/App.java
+++ src/App.java
@@ -3,22 +3,27 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        while(true) { // break 걸리기 전까지 반복
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int a = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int b = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            // 문자열이 아닌 문자 입력받기
+            char tool = sc.next().charAt(0);
+            int result = 0;
+            switch (tool) { // 각 사칙연산에 해당하는 작업 수행
+                case '+' -> result = a + b;
+                case '-' -> result = a - b;
+                case '/' -> result = a / b;
+                case '*' -> result = a * b;
+            }
+            /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+            System.out.println("결과: " + result);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int a = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int b = sc.nextInt();
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        // 문자열이 아닌 문자 입력받기
-        char tool = sc.next().charAt(0);
-        int result = 0;
-        switch(tool) { // 각 사칙연산에 해당하는 작업 수행
-            case '+' -> result = a + b;
-            case '-' -> result = a - b;
-            case '/' -> result = a / b;
-            case '*' -> result = a * b;
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String ans = sc.next();
+            if (ans.equals("exit")) break;
         }
-        /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
-        System.out.println("결과: " + result);
     }
 }
