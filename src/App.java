f3afb32074137d8a61707730b1649a43e28f64fd
GaEun1216 <gani6382@gmail.com>
2024-04-26 16:34:28 +0900

    Level 1-2

--- src/App.java
+++ src/App.java
@@ -8,5 +8,8 @@ public class App {
         int a = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
         int b = sc.nextInt();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        // 문자열이 아닌 문자 입력받기
+        char c = sc.next().charAt(0);
     }
 }
