e803d3ee56dd0bfc06706a3cdb7780d161ebc546
GaEun1216 <gani6382@gmail.com>
2024-04-26 17:01:40 +0900

    Level 1-8 : foreach문 추가

--- src/App.java
+++ src/App.java
@@ -9,7 +9,7 @@ public class App {
         Queue<Integer> que = new LinkedList<Integer>();
         /* 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언 */
         int index = 0;
-
+        String[] ans = new String[3];
         while(true) { // break 걸리기 전까지 반복
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int a = sc.nextInt();
@@ -27,15 +27,19 @@ public class App {
             }
             /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
             System.out.println("결과: " + result);
-
             que.add(result);
+            // 1. remove 로 첫번째 값 지우기
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String ans1 = sc.next();
-            // poll 함수는 가장 첫번째값 제거
-            if (ans1.equals("remove")) que.poll();
+            ans[0] = sc.next();
+            if (ans[0].equals("remove")) que.poll();
+            // 2. 지금까지 연산 조회
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            ans[1] = sc.next();
+            if (ans[1].equals("inquiry")) que.forEach(data -> System.out.println(data));
+            // 3. while문 빠져 나가기
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String ans2 = sc.next();
-            if (ans2.equals("exit")) break;
+            ans[2] = sc.next();
+            if (ans[2].equals("exit")) break;
 
         }
     }
