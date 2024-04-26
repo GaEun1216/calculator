a1bb315c6b0f3f218ba99974bea3eab92b1df427
GaEun1216 <gani6382@gmail.com>
2024-04-26 17:34:18 +0900

    Level 2-2 이전 커밋과 동일

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,7 +5,6 @@ public class Calculator {
     Queue<Integer> que = new LinkedList<Integer>();
     public int calculate(){return 0;}
     public int calculate(int a,int b,char tool) throws CalException {
-        Scanner sc = new Scanner(System.in);
         int result = 0;
         switch (tool) { // 각 사칙연산에 해당하는 작업 수행
             case '+' -> result = a + b;
@@ -17,14 +16,6 @@ public class Calculator {
             case '*' -> result = a * b;
         }
         que.add(result);
-        /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
-        System.out.println("결과: " + result);
-        // 1. remove 로 첫번째 값 지우기
-        System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-        if (sc.next().equals("remove")) que.poll();
-        // 2. 지금까지 연산 조회
-        System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-        if (sc.next().equals("inquiry")) que.forEach(data -> System.out.println(data));
         return result;
     }
 }
