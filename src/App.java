1524119c4470c392424e70ffa1e2a05719d6592a
GaEun1216 <gani6382@gmail.com>
2024-04-29 17:35:57 +0900

    Level 2-7 원의 넓이 추가

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class App {
@@ -5,31 +6,42 @@ public class App {
         Calculator cal = new Calculator();
         Scanner sc = new Scanner(System.in);
         String[] ans = new String[3];
-        int result;
+        int key;
 
         while(true) { // break 걸리기 전까지 반복
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int a = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int b = sc.nextInt();
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            // 문자열이 아닌 문자 입력받기
-            char tool = sc.next().charAt(0);
-            result = cal.calculate(a,b,tool);
+            System.out.print("사칙 연산 => 1 , 원의 넓이 계산 => 2 \n입력 : ");
+            key = sc.nextInt();
+            if(key ==1) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int a = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int b = sc.nextInt();
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                // 문자열이 아닌 문자 입력받기
+                char tool = sc.next().charAt(0);
 
-            /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
-            System.out.println("결과: " + result);
+                System.out.println("결과: " + cal.calculate(a, b, tool));
 
-            // 1. remove 로 첫번째 값 지우기
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            ans[0] = sc.next();
+                // 1. remove 로 첫번째 값 지우기
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                ans[0] = sc.next();
 
-            if (ans[0].equals("remove")) cal.removeResult();
+                if (ans[0].equals("remove")) cal.removeResult();
 
-            // 2. 지금까지 연산 조회
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            ans[1] = sc.next();
-            if (ans[1].equals("inquiry")) cal.inquiryResults();
+                // 2. 지금까지 연산 조회
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                ans[1] = sc.next();
+                if (ans[1].equals("inquiry")) cal.inquiryResults();
+            }
+            else if(key == 2){
+                System.out.print("넓이를 구할 원의 반지름을 입력하세요 : ");
+                int a = sc.nextInt();
+                System.out.println("원의 넓이 : "+cal.calradius(a));
+                // 연산 조회
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                ans[1] = sc.next();
+                if (ans[1].equals("inquiry")) cal.inquiryResults2();
+            }
             // 3. while문 빠져 나가기
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             ans[2] = sc.next();
--- src/Calculator.java
+++ src/Calculator.java
@@ -2,10 +2,15 @@ import java.util.*;
 
 public class Calculator {
     /* 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다. */
+
+    // 원주율은 바뀌지 않는 값이므로 static final 로 저장
+    static final double PI = 3.141592;
     private Queue<Integer> que;
+    private Queue<Double> que2;
     public Calculator(){
         // 컬랙션 초기화
         que = new LinkedList<Integer>();
+        que2 = new LinkedList<Double>();
     }
     public int calculate(int a,int b,char tool) throws CalException {
         int result = 0;
@@ -22,6 +27,13 @@ public class Calculator {
         return result;
     }
 
+    public double calradius(int r){
+        double result = r*r*PI;
+        que2.add(result);
+        return result;
+    }
+
+
     public Queue getque(){
         return que;
     }
@@ -30,11 +42,23 @@ public class Calculator {
         que = q;
     }
 
+    public Queue getque2(){
+        return que2;
+    }
+
+    public void setque2(Queue q){
+        que2 = q;
+    }
+
     public void removeResult(){
         que.remove();
     }
     public void inquiryResults() {
         que.forEach(data -> System.out.println(data));
+    }
+
+    public void inquiryResults2() {
+        que2.forEach(data -> System.out.println(data));
     }
 
 
