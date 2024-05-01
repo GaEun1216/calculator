0c217d0b139d82a62b72b1016e4196d26350af32
GaEun1216 <gani6382@gmail.com>
2024-05-01 12:04:42 +0900

    Level 3-1

--- src/ArithmeticCalculator.java
+++ src/ArithmeticCalculator.java
@@ -8,16 +8,18 @@ public class ArithmeticCalculator extends Calculator{
      */
     // 인터페이스 abs를 선언하고 switch문을 이용하여 원하는 기능을 가져온다.
     private AbstractOperation abs;
-
+    OperatorType operate;
 
     public double cal(int a, int b,char tool) throws CalException{
         double result = 0;
-        switch (tool) { // 각 사칙연산에 해당하는 작업 수행
-            case '+' -> abs = new AddOperation();
-            case '-' -> abs = new SubstractOperation();
-            case '/' -> abs = new DivideOperation();
-            case '*' -> abs = new MultiplyOperation();
-            case '%' -> abs =  new ModOperator(); //추가
+        operate = OperatorType.createOperate(tool);
+        // 기존 switch 인자는  tool 인데 operate로 변환
+        switch (operate) { // 각 사칙연산에 해당하는 작업 수행
+            case Add -> abs = new AddOperation();
+            case Subtract -> abs = new SubstractOperation();
+            case Divide -> abs = new DivideOperation();
+            case Multiply -> abs = new MultiplyOperation();
+            case Mod -> abs =  new ModOperator(); //추가
         }
         // 분모가 0일 경우 오류 발생 메시지 출력
         if(b==0) throw new CalException();
--- src/OperatorType.java
+++ src/OperatorType.java
@@ -7,12 +7,27 @@ public enum OperatorType{
 
     Divide('/'),
 
-    Mod('%');
+    Mod('%'),
 
+    None(' ');
+    // 주어진 연산기호가 아닌 다른 기호가 들어왔을 때 처리
+
+    // enum변수를 담아줄 변수명
     private char sign;
 
     OperatorType(char sign) {
         this.sign = sign;
     }
-    
+
+    //char 형태를 enum으로 반환해준다
+    public static OperatorType createOperate(char sign){
+        // 사칙연산 클래스에서 기호에 해당하는 값을 받았을 때 반환할 인자
+        if(sign == Add.sign) return Add;
+        if(sign == Subtract.sign) return Subtract;
+        if(sign == Divide.sign) return Divide;
+        if(sign == Mod.sign) return Mod;
+        if(sign == Multiply.sign) return Multiply;
+        return None;
+    }
+
 }
